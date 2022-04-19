31051849af33a30fed16bd4655716884426095da
djmambos <frozeefrozee12345@gmail.com>
2022-04-20 01:12:14 +0400

    fix

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -12,41 +12,39 @@ class Test extends BaseController
     public const MAX_ITEM_COUNT = 100;
 
     public static function index() {
-        echo 'Start getItemsFromSteam command';
+        echo 'Start getItemsFromSteam command' . PHP_EOL;
 
         $items = [];
 
-        for ($start = 0; $start <= 10000; $start += self::MAX_ITEM_COUNT) {
+        $requestCount = 1;
+
+        $totalPagesCount = self::getTotalPagesCount();
+
+        for ($start = 0; $start <= $totalPagesCount; $start += self::MAX_ITEM_COUNT) {
             do {
-                $response = Http::get('https://steamcommunity.com/market/search/render/?query=&start=' . $start . '&count=' . self::MAX_ITEM_COUNT . '&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any');
-                sleep(15);
+                echo '[Request counts: ' . $requestCount . '] Make steam request. Limit = ' . $start . PHP_EOL;
+                $requestCount++;
+
+                $response = Http::get('https://steamcommunity.com/market/search/render/?query=&norender=1&start=' . $start . '&count=' . self::MAX_ITEM_COUNT . '&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any');
+
+                sleep(10);
             } while ($response->failed());
 
-            $html = $response->json()['results_html'];
+            $result = $response->json()['results'];
 
-            $dom = new Dom();
-            $dom->setOptions(
-                ['strict' => TRUE]
-            );
-            $dom->loadStr($response->json()['results_html']);
-            $itemRows = $dom->getElementsByClass('market_listing_row');
-
-            $i = 0;
-
-            foreach ($itemRows as $row) {
-                $itemDom = new Dom();
-                $itemDom->loadStr($row->outerHtml());
-
-                $itemName = $itemDom->getElementsByClass('market_listing_item_name')->text;
-                $itemQuantity = $itemDom->getElementsByClass('market_listing_num_listings_qty')->text;
-                $itemPrice = substr(explode(' ', $itemDom->getElementsByClass('sale_price')->text)[0], 1);
-//            $itemImg = $itemDom->getElementsByTag('#result_' . $i . '_image')->text;
+            foreach ($result as $item) {
+                $itemName = $item['name'];
+                $itemQuantity = $item['sell_listings'];
+                $itemPrice = $item['sell_price'];
+                $itemType = $item['asset_description']['type'];
+                $itemImg = $item['asset_description']['icon_url'];
 
                 $items[] = [
                     'name' => $itemName,
                     'quantity' => $itemQuantity,
                     'price' => $itemPrice,
-//                'image' => $itemImg
+                    'type' => $itemType,
+                    'image' => $itemImg
                 ];
 
                 echo '[itemName] ' . $itemName . PHP_EOL;
@@ -56,5 +54,18 @@ class Test extends BaseController
         echo '<pre>';
         print_r($items);
         die;
+    }
+
+    private static function getTotalPagesCount(): int {
+        $requestCount = 1;
+
+        do {
+            echo '[ ' . __METHOD__ . ' ][Request counts: ' . $requestCount . '] Make steam request' . PHP_EOL;
+            $requestCount++;
+            $response = Http::get('https://steamcommunity.com/market/search/render/?query=&norender=1&start=' . 0 . '&count=' . self::MAX_ITEM_COUNT . '&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any');
+            sleep(10);
+        } while ($response->failed());
+
+        return $response->json()['total_count'];
     }
 }
